2d135535363501909bbe3a422cd9de971683d224
hjq133 <1399615054@qq.com>
2020-08-11 01:26:40 +0800

    modify url and base template

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'', TemplateView.as_view(template_name="index.html")),  # 这里将url的根路径指向vue中的index页面
 ]
