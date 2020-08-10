b64cfb791f0e3af4ef5823df9c39aee498277ff8
hjq133 <1399615054@qq.com>
2020-08-11 02:07:36 +0800

    add cookie.js

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,30 +1,69 @@
 <template>
-  <div class="login_container" background>
+  <div
+    class="login_container"
+    background
+  >
     <div class="login_box">
       <!-- 头像区域 -->
       <div class="avatar_box">
-        <img src="../assets/logo.png" alt />
+        <img
+          src="../assets/logo.png"
+          alt
+        >
       </div>
       <!-- 登录表单区域 -->
-      <el-form :model="loginForm" class="login_form" ref="loginForm" label-width="0px">
+      <el-form
+        ref="loginForm"
+        :model="loginForm"
+        class="login_form"
+        label-width="0px"
+      >
         <!-- 用户名 -->
-        <el-form-item label prop="username">
-          <el-input v-model="loginForm.username" prefix-icon="fa fa-user" placeholder="用户名"></el-input>
+        <el-form-item
+          label
+          prop="username"
+        >
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="fa fa-user"
+            placeholder="用户名"
+          />
         </el-form-item>
         <!-- 密码 -->
-        <el-form-item label prop="password">
+        <el-form-item
+          label
+          prop="password"
+        >
           <el-input
             v-model="loginForm.password"
             show-password
             prefix-icon="fa fa-lock"
             placeholder="密码"
-          ></el-input>
+          />
         </el-form-item>
         <!-- 按钮区域 -->
-        <el-form-item class="btns" label>
-          <el-button type="primary" @click="login">登录</el-button>
-          <el-button type="primary" @click="toregister">注册</el-button>
-          <el-button type="info" @click="reset">重置</el-button>
+        <el-form-item
+          class="btns"
+          label
+        >
+          <el-button
+            type="primary"
+            @click="login"
+          >
+            登录
+          </el-button>
+          <el-button
+            type="primary"
+            @click="toregister"
+          >
+            注册
+          </el-button>
+          <el-button
+            type="info"
+            @click="reset"
+          >
+            重置
+          </el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -32,50 +71,45 @@
 </template>
 
 <script>
-import axios from "axios";
-import Qs from "qs";
+import axios from 'axios'
+import Qs from 'qs'
 export default {
-  data() {
+  data () {
     return {
       // 登录表单的数据绑定对象
       loginForm: {
-        username: "",
-        password: ""
+        username: '',
+        password: ''
       }
-    };
+    }
   },
   methods: {
-    tohome() {
-      this.$router.push("/home");
+    tohome () {
+      this.$router.push('/home')
     },
-    toregister() {
-      this.$router.push("/register");
+    toregister () {
+      this.$router.push('/register')
     },
-    reset() {
-      this.$refs.loginForm.resetFields();
+    reset () {
+      this.$refs.loginForm.resetFields()
     },
     // 登录
-    login() {
-      this.$refs.validate((valid)=>{
-        if(valid){
-          var data=Qs.stringify({"username":this.loginForm.username,"password":this.loginForm.password})
+    login () {
+      this.$refs.validate((valid) => {
+        if (valid) {
+          var data = Qs.stringify({ username: this.loginForm.username, password: this.loginForm.password })
           // 向后端发送请求
-          axios.post("http://localhost:8000",data).then(
-            function(resp) {
-              const flag=resp.data.request['flag']
-              if(flag=='yes')
-                this.$router.push("/home")
-              else
-                alert(resp.data.request['msg'])
+          axios.post('http://localhost:8000', data).then(
+            function (resp) {
+              const flag = resp.data.request.flag
+              if (flag == 'yes') { this.$router.push('/home') } else { alert(resp.data.request.msg) }
             }
           )
-        }
-        else
-          alert("出现错误，请重试")
+        } else { alert('出现错误，请重试') }
       })
     }
   }
-};
+}
 </script>
 
 <style lang="less" scoped>
--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -1,28 +1,74 @@
 <template>
-  <div class="register_container" background>
+  <div
+    class="register_container"
+    background
+  >
     <div class="register_box">
       <!-- 头像区域 -->
       <div class="avatar_box">
-        <img src="../assets/logo.png" alt />
+        <img
+          src="../assets/logo.png"
+          alt
+        >
       </div>
       <!-- 注册表单区域 -->
-      <el-form :model="registerForm" class="register_form" ref="registerForm" label-width="0px">
+      <el-form
+        ref="registerForm"
+        :model="registerForm"
+        class="register_form"
+        label-width="0px"
+      >
         <!-- 用户名 -->
-        <el-form-item label prop="username">
-          <el-input v-model="registerForm.username" prefix-icon="fa fa-user" placeholder="用户名"></el-input>
+        <el-form-item
+          label
+          prop="username"
+        >
+          <el-input
+            v-model="registerForm.username"
+            prefix-icon="fa fa-user"
+            placeholder="用户名"
+          />
         </el-form-item>
         <!-- 邮箱 -->
-        <el-form-item label prop="email">
-          <el-input v-model="registerForm.email" prefix-icon="fa fa-envelope" placeholder="邮箱"></el-input>
+        <el-form-item
+          label
+          prop="email"
+        >
+          <el-input
+            v-model="registerForm.email"
+            prefix-icon="fa fa-envelope"
+            placeholder="邮箱"
+          />
         </el-form-item>
         <!-- 密码 -->
-        <el-form-item label prop="password">
-          <el-input v-model="registerForm.password" show-password prefix-icon="fa fa-lock" placeholder="密码"></el-input>
+        <el-form-item
+          label
+          prop="password"
+        >
+          <el-input
+            v-model="registerForm.password"
+            show-password
+            prefix-icon="fa fa-lock"
+            placeholder="密码"
+          />
         </el-form-item>
         <!-- 按钮区域 -->
-        <el-form-item class="btns" label>
-          <el-button type="primary" @click="register">注册</el-button>
-          <el-button type="info" @click="reset">重置</el-button>
+        <el-form-item
+          class="btns"
+          label
+        >
+          <el-button
+            type="primary"
+            @click="register"
+          >
+            注册
+          </el-button>
+          <el-button
+            type="info"
+            @click="reset"
+          >
+            重置
+          </el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -31,47 +77,46 @@
 
 <script>
 export default {
-  data() {
+  data () {
     return {
       // 注册表单的数据绑定对象
       registerForm: {
-        username: "",
-        password: "",
-        email:""
-      },
-    };
+        username: '',
+        password: '',
+        email: ''
+      }
+    }
   },
-  methods:{
-    tologin(){
-      this.$router.push("/login");
+  methods: {
+    tologin () {
+      this.$router.push('/login')
     },
-    reset() {
-      this.$refs.registerForm.resetFields();
+    reset () {
+      this.$refs.registerForm.resetFields()
     },
-    register() {
-      this.$refs.validate((valid)=>{
-        if(valid){
-          var data=Qs.stringify({"username":this.registerForm.username,
-          "email":this.registerForm.email, "password":this.registerForm.password})
+    register () {
+      this.$refs.validate((valid) => {
+        if (valid) {
+          var data = Qs.stringify({
+            username: this.registerForm.username,
+            email: this.registerForm.email,
+            password: this.registerForm.password
+          })
           // 向后端发送数据
-          axios.post("http://localhost:8000",data).then(
-            function(resp) {
-              const flag=resp.data.request['flag']
-              if(flag=='yes')
-                this.$router.push("/login")
-              else {
-                alert(resp.data.request['msg'])
+          axios.post('http://localhost:8000', data).then(
+            function (resp) {
+              const flag = resp.data.request.flag
+              if (flag == 'yes') { this.$router.push('/login') } else {
+                alert(resp.data.request.msg)
                 this.reset()
               }
             }
           )
-        }
-        else
-          alert("出现错误，请重试")
+        } else { alert('出现错误，请重试') }
       })
     }
   }
-};
+}
 </script>
 
 <style lang="less" scoped>
