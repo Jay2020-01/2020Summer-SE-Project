a8744cffa3e3fac8654680d702aaceaae728fba2
hjq133 <1399615054@qq.com>
2020-08-12 20:20:49 +0800

    rm api app

--- diamond/backend/models.py
+++ diamond/backend/models.py
@@ -1,6 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
+
+# from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(models.Model):
+    # SEX_CHOICES = (
+    #     ('男', 1),
+    #     ('女', 0),)
+    # id = models.CharField(max_length=64,verbose_name="id")
+    username = models.CharField(max_length=64, verbose_name="姓名")
+    password = models.CharField(max_length=256, verbose_name="密码")
 
 # class User(AbstractUser):
 #     """
--- diamond/backend/views.py
+++ diamond/backend/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+from .models import User
+
+
+# Create your views here.
+def change_info(request):
+    print('change_info')
+    username = request.POST.get("username")
+    print(username)
+    data = {'what': True}
+    return JsonResponse(data)
