2e03e2254a1f3f6710a355c829d7d9b89b5da2b7
hjq133 <1399615054@qq.com>
2020-08-13 21:39:42 +0800

    mofify urls.py

--- diamond/diamond/urls.py
+++ diamond/diamond/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from rest_framework.authtoken import views as token_views
 import login.views as login_views
 import backend.views as backend_views
 urlpatterns = [
     # admin
     path('admin/', admin.site.urls),
-    
+    path('api-token-auth/', token_views.obtain_auth_token),
     # index.html App.vue
     path('', TemplateView.as_view(template_name="index.html")),
     path('ajax/change_info/', backend_views.change_info, name='change_info'),
