c665cc2ddc0cc708b659799545292a7d28a856c6
terminator-128 <52963089+terminator-128@users.noreply.github.com>
2020-08-13 21:46:36 +0800

    数据库更新
    请务必下载全后端第三方库

--- diamond/diamond/settings.py
+++ diamond/diamond/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'rest_framework_swagger',
-    # 'guardian',
+    'guardian',
 
     # customed App
     'backend.apps.BackendConfig',
