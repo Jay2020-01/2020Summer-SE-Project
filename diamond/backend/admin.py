374c591be3a6fbc8767915c639e85e32470095a0
hjq133 <1399615054@qq.com>
2020-08-13 21:45:10 +0800

    mofify login views.py

--- diamond/backend/admin.py
+++ diamond/backend/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-# from .models import User
+# from django.contrib.auth.admin import UserAdmin
+from .models import UserProfile
 
 # Register your models here.
 
-# admin.site.register(User, UserAdmin)
+admin.site.register(UserProfile)
