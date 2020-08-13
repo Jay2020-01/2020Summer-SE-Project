d189f4e95dc6a70aabcc52c7a962fc507af2e4a0
hjq133 <1399615054@qq.com>
2020-08-14 00:49:16 +0800

    add change info

--- diamond/login/views.py
+++ diamond/login/views.py
@@ -2,7 +2,7 @@ import requests
 from django.shortcuts import render
 from django.db.models.signals import post_save
 from django.http import JsonResponse
-from backend.models import User
+from backend.models import User, UserProfile
 from rest_framework.authtoken.models import Token
 from django.conf import settings
 from rest_framework.authtoken import views
@@ -37,8 +37,8 @@ def register(request):
     password = request.POST.get("password")
     data = {'token': None, 'user': username}
     if not User.objects.filter(username=username):
-        user = User(username=username, email=mail_address, password=password)
-        user.save()
+        user = User.objects.create(username=username, email=mail_address, password=password)
+        UserProfile.objects.create(user=user)
         token = Token.objects.get(user=user)
         data = {'token': str(token), 'user': username}
     return JsonResponse(data)
