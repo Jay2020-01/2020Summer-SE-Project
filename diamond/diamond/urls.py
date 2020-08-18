afdd9e6eaa6d39d577d0ae661fe1eba366a10ab9
terminator-128 <52963089+terminator-128@users.noreply.github.com>
2020-08-18 14:56:36 +0800

    添加 is_leader URL

--- diamond/diamond/urls.py
+++ diamond/diamond/urls.py
@@ -58,6 +58,7 @@ urlpatterns = [
     path('ajax/delete_team_member/', team_views.delete_team_member, name="delete_team_member"),
     path('ajax/exit_team/', team_views.exit_team, name="delete_team_member"),
     path('ajax/get_team_docs/', team_views.get_team_docs, name="get_team_docs"),
+    path('ajax/is_leader/', team_views.is_leader, name="is_leader"),
     # backend api
 ]
 
